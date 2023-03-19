cef94f326ddab78a643e7649a9b423f92a8612ad
Shivanirathor <Shivanirathor034@gmail.com>
2023-03-20 00:44:12 +0530

    project

--- App.js
+++ App.js
@@ -1,74 +1,66 @@
+var savedResult = [];
 
-function savedbtn(){
-    console.log("working save btn")
-    var wet =  document.querySelector(".inputBox1").innerHTML;
-    var set =  document.querySelector(".inputBox2").innerHTML;
-
-    localStorage.setItem(wet,set);
-    console.log(localStorage.getItem(set));
-    set++;
+function savedbtn() {
+ console.log("savedresult", savedResult);
+localStorage.setItem("savedResult" ,JSON.stringify(savedResult));
+  // localStorage.setItem(wet,set);
+  // localStorage.setItem("value",5)
+  // let result = localStorage.getItem("value")
+  // console.log(result);
+  // console.log(localStorage.getItem(set));
+  // set++;
 }
 
-var hcount = 0 
-function historybtn(){
-    console.log("working history btn")
-    hcount++;
-    if(hcount%2==1){
-        document.querySelector(".historycon").style.display = "block";
-        var data = document.querySelector('.historycon');
+var hcount = 0;
+function historybtn() {
+//   localStorage.setItem("Name", "shivani");
+//   console.log("working history btn");
+  hcount++;
+  if (hcount % 2 == 1) {
+    document.querySelector(".historycon").style.display = "block";
+    var data = document.querySelector(".historycon");
 
-        for (p in localStorage) {
-            var ans = localStorage.getItem(p);
-            if (ans != null) {
-                // console.log(ans);
-                var h3 = document.createElement("h3");
-                h3.setAttribute('class', 'remove')
-
-                h3.innerHTML = `${p} => Result = ${ans}`;
-                console.log(h3);
-                data.appendChild(h3);
-
-            }
-        }
-    }else{
-        document.querySelector(".historycon").style.display = "none";
-        var rem = document.querySelectorAll('.remove');
-        console.log(rem);
-        for (var i = 0; i < rem.length; i++) {
-            rem[i].remove();
-        }
+    for (p in localStorage) {
+      var ans = localStorage.getItem(p);
+      if (ans != null) {
+        // console.log(ans);
+        var h3 = document.createElement("h3");
+        h3.setAttribute("class", "remove");
+        h3.innerHTML = `${ans}`;
+        // <h3 class="remove"> value=> Result = 5 </h3>
+        console.log(h3);
+        data.appendChild(h3);
+      }
     }
+  } else {
+    document.querySelector(".historycon").style.display = "none";
+    var rem = document.querySelectorAll(".remove");
+    console.log("printing", rem);
+    for (var i = 0; i < rem.length; i++) {
+      rem[i].remove();
+    }
+  }
 }
 
-function searchBtn(){
-    event.preventDefault();
-
-    console.log("working search btn")
-   var problem = document.getElementById('problemBar').value;
-   var category = document.getElementById('categoryBar').value;
-
-   let pro = fetch(`https://newton.vercel.app/api/v2/${category}/${problem}`)
-   console.log(pro)
-   pro.then((reponse) => {
-    console.log(reponse.status)
-    console.log(reponse.ok)
-    return reponse.json()
-   }).then((value) => {
-    console.log(value);
-    var input2 =  value.result;
-    document.querySelector(".inputBox2").innerHTML = input2;
-   })
-   
-
-   const Input1  = document.querySelector(".inputBox1").innerHTML = category+" :"+problem;
-   var set =  document.querySelector(".inputBox1").value;
-   var wet =  document.querySelector(".inputBox2").innerHTML;
-   localStorage.setItem(set,wet);
+function deleteBtn() {
+  console.log("working delete btn");
+  document.querySelector(".inputBox1").innerHTML = "";
 }
 
+function searchBtn() {
+  let expression = document.getElementById("problemBar").value;
+  console.log(expression);
+  let formula = document.getElementById("categoryBar").value;
+  console.log(formula);
 
-function deleteBtn(ele){
-    console.log("working delete btn")
-    document.querySelector(".inputBox1").innerHTML = "";
-    document.querySelector(".inputBox2").innerHTML = "";
+  let result = fetch(`https://newton.now.sh/api/v2/${formula}/${expression}`);
+  result.then((valueA) => {
+    return valueA.json();
+  }).then((valueB) => {
+     console.log(valueB.result);
+     savedResult.push(valueB)
+     document.querySelector(".inputBox1").innerHTML=valueB.result;
+  });
+
+
 }
