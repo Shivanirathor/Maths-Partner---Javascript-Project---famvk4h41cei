fe9d63f3944654a354bdfa3a97b76af4e0843fed
Shivanirathor <Shivanirathor034@gmail.com>
2023-07-15 20:10:04 +0530

    mock

--- App.js
+++ App.js
@@ -1,47 +1,62 @@
 var savedResult = [];
 
 function searchBtn() {
-  let expression = document.getElementById("problemBar").value; 
+  let expression = document.getElementById("problemBar").value;
   let formula = document.getElementById("categoryBar").value;
-  
+
   let result = fetch(`https://newton.now.sh/api/v2/${formula}/${expression}`);
 
-  result.then((valueA) => {
-    return valueA.json();
-  }).then((valueB) => {
-    //  console.log(valueB.result);
-     savedResult.push(valueB)
-     document.querySelector(".inputBox1").innerHTML= ` So, The Result is ${valueB.result}`;
-  });
+  result
+    .then((valueA) => {
+      return valueA.json();
+    })
+    .then((valueB) => {
+      //  console.log(valueB.result);
+      savedResult.push(valueB);
+      document.querySelector(
+        ".inputBox1"
+      ).innerHTML = ` So, The Result is ${valueB.result}`;
+    });
 }
 
-
 function savedbtn() {
-//  console.log("savedresult", savedResult);
-localStorage.setItem("savedResult" ,JSON.stringify(savedResult)); 
- 
+  const outputValue = document.querySelector(".inputBox1").innerHTML;
+  // console.log("outputValue", outputValue);
+  if (outputValue !== "Output Box...") {
+    localStorage.setItem("savedResult", JSON.stringify(savedResult));
+    alert("Success: Your solution is saved");
+  } else {
+    alert("Error: Solution not found!!");
+  }
 }
 
 var hcount = 0;
 function historybtn() {
-//   localStorage.setItem("Name", "shivani");
-//   console.log("working history btn");
   hcount++;
   if (hcount % 2 == 1) {
     document.querySelector(".historycon").style.display = "block";
     var data = document.querySelector(".historycon");
 
-    for (p in localStorage) {
-      var ans = localStorage.getItem(p);
+    for (var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      var ans = localStorage.getItem(key);
       if (ans != null) {
-  
-        // console.log(ans);
+
+        var container = document.createElement("div");
         var h3 = document.createElement("h3");
+        var btn = document.createElement("button");
+        console.log(btn)
+       btn.innerHTML = "delete";
+        btn.classList.add("remove");
+        btn.addEventListener("click", (event) => {
+          event.target.parentElement.remove();
+          localStorage.clear();
+        });
+
         h3.setAttribute("class", "remove");
-        h3.innerHTML = `${ans}`;
-        // <h3 class="remove"> value=> Result = 5 </h3>
-        // console.log(h3);
-        data.appendChild(h3);
+        h3.innerHTML = ans;
+        container.append(h3, btn);
+        data.appendChild(container);
       }
     }
   } else {
